3aa3548e7f7bba0790bf20dfe257a3b769c283c6
TarCarbon <metalcarbon58@gmail.com>
2023-11-15 21:34:52 +0100

    m

--- src/main/java/org/example/serviceHTTP/HttpStatusChecker.java
+++ src/main/java/org/example/serviceHTTP/HttpStatusChecker.java
@@ -1,28 +1,25 @@
 package org.example.serviceHTTP;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 
 public class HttpStatusChecker {
 
-    public static String getStatusImage(int code) {
-        try {
-            URI uri = new URI("https", "http.cat", ( "/".concat(String.valueOf(code)).concat(".jpg") ), null);
-            URL url = uri.toURL();
+    public String getStatusImage(int code) throws IOException {
 
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            int responseCode = connection.getResponseCode();
-            if(responseCode != 404) {
-                    return uri.getScheme().concat("://").concat(uri.getHost()).concat(uri.getPath());
-                } else throw new Exception() ;
+        String str = "https://http.cat/" + code + ".jpg";
+        URL url = new URL(str);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (httpURLConnection.getResponseCode() == 200) {
+            return httpURLConnection.getURL().toString();
+        } else {
+            throw new IOException("Non-existent code");
         }
-        return null;
     }
 }
